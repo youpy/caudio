d7191d5458f1b3a9057357b33f9e53c2c68649a5
youpy <youpy@buycheapviagraonlinenow.com>
2020-07-09 09:13:00 +0900

    add build constraints

--- callback.go
+++ callback.go
@@ -1,45 +1,4 @@
 package caudio
 
-import "C"
-import (
-	"sync"
-)
-
-//export baudio_callback
-func baudio_callback(index C.int, t C.float, stepCount C.int) C.float {
-	fn := lookup(int(index))
-
-	return C.float(fn(float64(t), int(stepCount)))
-}
-
 // Callback is a function that the user registers with the `Audio` instance to make a sound
 type Callback func(t float64, i int) float64
-
-// based on https://github.com/golang/go/wiki/cgo#function-variables
-
-var mu sync.Mutex
-var index int
-var fns = make(map[int]Callback)
-
-func register(fn Callback) int {
-	mu.Lock()
-	defer mu.Unlock()
-	index++
-	for fns[index] != nil {
-		index++
-	}
-	fns[index] = fn
-	return index
-}
-
-func lookup(i int) Callback {
-	mu.Lock()
-	defer mu.Unlock()
-	return fns[i]
-}
-
-func unregister(i int) {
-	mu.Lock()
-	defer mu.Unlock()
-	delete(fns, i)
-}
---
+++ callback_darwin.go
@@ -0,0 +1,42 @@
+package caudio
+
+import "C"
+import (
+	"sync"
+)
+
+//export baudio_callback
+func baudio_callback(index C.int, t C.float, stepCount C.int) C.float {
+	fn := lookup(int(index))
+
+	return C.float(fn(float64(t), int(stepCount)))
+}
+
+// based on https://github.com/golang/go/wiki/cgo#function-variables
+
+var mu sync.Mutex
+var index int
+var fns = make(map[int]Callback)
+
+func register(fn Callback) int {
+	mu.Lock()
+	defer mu.Unlock()
+	index++
+	for fns[index] != nil {
+		index++
+	}
+	fns[index] = fn
+	return index
+}
+
+func lookup(i int) Callback {
+	mu.Lock()
+	defer mu.Unlock()
+	return fns[i]
+}
+
+func unregister(i int) {
+	mu.Lock()
+	defer mu.Unlock()
+	delete(fns, i)
+}
